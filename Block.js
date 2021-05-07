efc34549be0df9b56f5408897a52c918ac7f8fa7
Dev-kanda <78482231+Dev-kanda@users.noreply.github.com>
2021-05-07 17:59:27 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -15,6 +15,7 @@ class Block{
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
+          angleMode(RADIANS);
         rectMode(CENTER);
         rect(0, 0, this.width, this.height);
         pop();
